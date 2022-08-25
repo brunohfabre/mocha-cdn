bf0de310857e361e920bcfe49709654ea6c8e9dd
Bruno Fabre <bruno.hfabre@gmail.com>
2022-08-24 22:49:47 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
